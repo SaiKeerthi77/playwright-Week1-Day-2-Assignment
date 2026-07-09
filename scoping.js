a6eada6955206b01f0df6669b19d9f3e3e24070f
SaiKeerthi77 <keerthisai9708@gmail.com>
2026-07-09 14:43:42 +0530

    Add files via upload
    Day 3 assignment -Scoping

---
+++ scoping.js
@@ -0,0 +1,17 @@
+let gender = "female"//global variable named female
+
+function PrintGender(){//created  function name as PrintGender
+
+let color ="brown"//declare function scope variable
+
+if(gender=="female"){
+var age =30
+let color = "pink"
+     console.log("color inside the block",color) 
+     
+}
+ console.log("print value of age ",age) 
+
+}
+PrintGender()
+console.log("the value of gender ", gender)
