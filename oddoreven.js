ec4238dbb388f5f1cda3f21bb2eaba133b942d57
SaiKeerthi77 <keerthisai9708@gmail.com>
2026-07-09 14:44:23 +0530

    Add files via upload
    Day 3-assignment -odd or even

---
+++ oddoreven.js
@@ -0,0 +1,11 @@
+function oddoreven(number){
+    var number = 12
+{
+  if (number % 2 === 0){
+    console.log("The given number is odd")
+}else{
+console.log("The given nuber is even")
+}
+}
+}
+oddoreven()
