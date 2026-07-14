767f9f45eef42d3cb317f63e2df8e7ddb43f4dd4
SaiKeerthi77 <keerthisai9708@gmail.com>
2026-07-14 14:18:43 +0530

    Add files via upload
    Day 5-Strings example 2

---
+++ Stringseg2.js
@@ -0,0 +1,13 @@
+//example 2
+String = " fly me to the moon "
+
+console.log(String.trim()) //Trim the String
+
+console.log(String.split("")) //Split the String into Words
+
+console.log(String.substring(15, 19))//Identify the Last Word
+
+var Lastword = "moon"
+console.log(Lastword.length)//the Length of the Last Word
+
+console.log(String.length)// return length of the string 
